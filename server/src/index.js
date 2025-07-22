dba47b2fcbbe5ccf7fef77f219a61c76ba45fd5c
drleavio <drlevio123@gmail.com>
2025-07-22 16:12:14 +0530

    otp feature added for login

--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,17 @@
 const express = require("express");
-const cors = require("cors"); // ✅ Import cors properly
+const cors = require("cors");
 require('dotenv').config({path:"../.env"});
 const userRoute=require("../userRoute/userRoute")
 const authRoute=require("../authRoute/authRoute")
+const authCheck=require("../middleware")
 const app=express();
 app.use(express.json());
 app.use(cors())
-// console.log(process.env.MONGO_URI);
 
 app.use("/api/auth",authRoute)
-app.use("/api",userRoute);
+app.use("/api",authCheck,userRoute);
 
 
 app.listen(5732,()=>{
-    console.log("listening");
-    
+    console.log("listening");   
 });
